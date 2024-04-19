b58e8c5a2de592b7c1f9778970ca642f1c58d100
Meryem İris <irismeryem.96@gmail.com>
2024-04-19 20:00:16 +0300

    add background

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let playerState = "idle";
+let playerState = "run";
 
 const dropdown = document.getElementById("animations");
 dropdown.addEventListener("change", (e) => {
@@ -10,15 +10,32 @@ dropdown.addEventListener("change", (e) => {
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const CANVAS_WIDTH = 600;
-const CANVAS_HEIGHT = 600;
+const CANVAS_WIDTH = 1920;
+const CANVAS_HEIGHT = 1080;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
-const playerImage = new Image();
-playerImage.src = "player.png";
+// Create background objects
+let gameSpeed = 5;
+const backGroundLayer1 = new Image();
+backGroundLayer1.src = "layer_01.png";
+const backGroundLayer2 = new Image();
+backGroundLayer2.src = "layer_02.png";
+const backGroundLayer3 = new Image();
+backGroundLayer3.src = "layer_03.png";
+const backGroundLayer4 = new Image();
+backGroundLayer4.src = "layer_04.png";
+const backGroundLayer5 = new Image();
+backGroundLayer5.src = "layer_05.png";
+
+// Create player objects
+
+const playerImg = new Image();
+
+playerImg.src = "player.png";
 const spriteWidth = 330;
 const spriteHeight = 221;
 let gameFrame = 0;
-
 const staggerFrames = 5;
 
 const spriteAnimation = [];
@@ -47,14 +64,20 @@ animationStates.forEach((state, index) => {
 
 function animate() {
 	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+	// Draw background layers
+	ctx.drawImage(backGroundLayer1, 0, 0);
+	ctx.drawImage(backGroundLayer2, 0, 0);
+	ctx.drawImage(backGroundLayer3, 0, 0);
+	ctx.drawImage(backGroundLayer4, 0, 0);
+	ctx.drawImage(backGroundLayer5, 0, 0);
+
+	// Draw player
 	let position = Math.floor(
 		(gameFrame / staggerFrames) % spriteAnimation[playerState].loc.length
 	);
 	let frameX = position * spriteWidth;
 	let frameY = spriteAnimation[playerState].loc[position].y;
-
-	// Draw the image
-
 	ctx.drawImage(
 		playerImage,
 		frameX,
@@ -62,17 +85,16 @@ function animate() {
 		spriteWidth,
 		spriteHeight,
 		0,
-		0,
+		700,
 		spriteWidth,
 		spriteHeight
 	);
-
 	if (gameFrame % staggerFrames === 0) {
 		if (frameX < 6) frameX++;
 		else frameX = 0;
 	}
-
 	gameFrame++;
+
 	requestAnimationFrame(animate);
 }
 
