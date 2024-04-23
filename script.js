40b2316b27b0f01d63ebdf6d162f1845644b65cc
Meryem İris <irismeryem.96@gmail.com>
2024-04-23 11:43:21 +0300

    update monster and enemies

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
 let gameSpeed = 0;
-const numberOfEnemies = 20;
+const numberOfEnemies = 3;
 const enemiesArray = []; // Array to store enemy objects
 
 // Set up speed slider
@@ -36,6 +36,7 @@ const backGroundLayer4 = new Image();
 backGroundLayer4.src = "./assets/layer_04.png";
 const backGroundLayer5 = new Image();
 backGroundLayer5.src = "./assets/layer_05.png";
+
 // Wait for images to load before starting the game
 window.addEventListener("load", () => {
 	class Layer {
@@ -72,27 +73,33 @@ window.addEventListener("load", () => {
 
 	const backgroundLayers = [layer1, layer2, layer3, layer4, layer5];
 
-	// Create enemy objects
+	// Create Enemies
 	const enemyImg = new Image();
 	enemyImg.src = "./assets/enemy.png";
 	class Enemy {
 		constructor() {
-			this.x = Math.random() * canvas.width;
-			this.y = Math.random() * canvas.height;
-			this.speed = Math.random() * 4 - 2;
 			this.enemyWidth = 136.5;
 			this.enemyHeight = 141;
+			this.speed = Math.random() * 5 + 1;
 			this.width = this.enemyWidth;
 			this.height = this.enemyHeight;
+			this.x = Math.random() * (canvas.width - this.width);
+			this.y = Math.random() * (canvas.height - this.height);
+			this.angle = Math.random() * 5 * Math.PI;
+			this.angleSpeed = Math.random() * 0.1;
+
 			this.frame = 0;
-			this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+			this.flapSpeed = Math.floor(Math.random() * 5 + 1);
 		}
 		update() {
-			this.x += this.speed;
-			this.y += this.speed;
+			this.x -= this.speed;
+			this.y += Math.sin(this.angle) * 2;
+			this.angle += this.angleSpeed;
+
+			if (this.x < 0) this.x = canvas.width;
+			if (this.y < 0) this.y = canvas.height - this.height;
 
 			// Animate enemies
-
 			if (gameFrame % this.flapSpeed === 0) {
 				this.frame >= 12 ? (this.frame = 0) : this.frame++;
 			}
@@ -113,30 +120,29 @@ window.addEventListener("load", () => {
 		}
 	}
 
-	// Create enemy objects
 	for (let i = 0; i < numberOfEnemies; i++) {
 		enemiesArray.push(new Enemy());
 	}
 
 	// Create Monster
-
 	const monsterImg = new Image();
-	monsterImg.src = "./assets/monster_walking.png";
+	monsterImg.src = "./assets/monster.png";
 
 	class Monster {
 		constructor() {
-			this.x = 1800;
-			this.y = 655;
+			this.x = 1920;
+			this.y = 600;
 			this.speed = 2;
-			this.monsterWidth = 655;
-			this.monsterHeight = 534;
-			this.width = this.monsterWidth * 0.5;
-			this.height = this.monsterHeight * 0.5;
+			this.monsterWidth = 262;
+			this.monsterHeight = 242;
+			this.width = this.monsterWidth * 1.5;
+			this.height = this.monsterHeight * 1.5;
 			this.frame = 0;
-			this.walkSpeed = 2;
+			this.walkSpeed = Math.floor(Math.random() * 3 + 1);
 		}
 		update() {
 			this.x -= this.speed;
+			if (this.x < -500) this.x = 1920;
 
 			// Animate monsters
 			if (gameFrame % this.walkSpeed === 0) {
@@ -148,7 +154,7 @@ window.addEventListener("load", () => {
 			ctx.drawImage(
 				monsterImg,
 				this.frame * this.monsterWidth,
-				0,
+				484,
 				this.monsterWidth,
 				this.monsterHeight,
 				this.x,
@@ -159,14 +165,13 @@ window.addEventListener("load", () => {
 		}
 	}
 
-	// Create monster
 	const monster = new Monster();
 
-	// Create player objects
+	// Create Player
 
 	const playerImg = new Image();
-
 	playerImg.src = "./assets/player.png";
+
 	const spriteWidth = 330;
 	const spriteHeight = 221;
 	let gameFrame = 0;
@@ -204,7 +209,7 @@ window.addEventListener("load", () => {
 			layer.draw();
 		});
 
-		// Draw player
+		// Update and Draw player
 		let position = Math.floor(
 			(gameFrame / staggerFrames) % spriteAnimation[playerState].loc.length
 		);
@@ -225,7 +230,6 @@ window.addEventListener("load", () => {
 			if (frameX < 6) frameX++;
 			else frameX = 0;
 		}
-		gameFrame++;
 
 		// Update and Draw enemies
 		enemiesArray.forEach((enemy) => {
@@ -238,6 +242,7 @@ window.addEventListener("load", () => {
 		monster.draw();
 
 		// Update game state
+		gameFrame++;
 		requestAnimationFrame(animate);
 	}
 
